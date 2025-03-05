a73f74ebb91fad0799e5a944ed6c6d08ec065b37
Madson Santos <madsonsamsung@gmail.com>
2025-03-05 15:57:39 -0300

    Organize the folders and install typescript

---
+++ src/services/cardService.ts
@@ -0,0 +1,27 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import fs from "fs";
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || "");
+
+export const CardDataAnalyzer = async () => {
+  try {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const prompt = `Detect and extract all relevant information from a Pokémon card in the provided image. The response should be structured in the following JSON format: { \"name\": \"Pokémon name\", \"stage\": \"Evolution stage (Basic, Stage 1, Stage 2)\", \"evolvesFrom\": \"Previous Pokémon name, if applicable\", \"hp\": \"Hit Points (HP)\", \"type\": \"Pokémon type (Grass, Fire, Water, etc.)\", \"ability\": { \"name\": \"Ability name, if applicable\", \"description\": \"Ability description, if applicable\" }, \"attacks\": [ { \"name\": \"Attack name\", \"energyCost\": [{ \"energy type (e.g., Grass)\": amount }], \"damage\": \"Attack damage (if available)\", \"description\": \"Attack description (if available)\" } ], \"weakness\": { \"type\": \"Weakness type (e.g., Fire)\", \"bonus\": \"Weakness bonus (e.g., +20)\" }, \"retreatCost\": \"Number of energy cards required to retreat\", \"number\": \"Card number within the set\", \"exRule\": \"Special rules for the card, if applicable (EX, GX, etc.)\" }`;
+    const imageData = fs.readFileSync("charizard.webp");
+
+    const imagePart = {
+      inlineData: {
+        data: imageData.toString("base64"),
+        mimeType: "image/webp",
+      },
+    };
+
+    const result = await model.generateContent([{ text: prompt }, imagePart]);
+
+    const responseText = await result.response.text();
+    console.log(responseText);
+  } catch (error) {
+    console.error("Erro ao gerar conteúdo:", error);
+  }
+};
